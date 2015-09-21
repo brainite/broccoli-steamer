31549c4f85ea95fcce54d3aca2a514ba3e99f8b9
Greg Payne <greg.payne@gmail.com>
2015-09-21 11:08:38 -0500

    Placeholders for CI testing

---
+++ example/Brocfile.js
@@ -0,0 +1,14 @@
+var steamer = require('broccoli-steamer')
+
+// Identify the files to watch.
+steamer.init
+  .dir("assets", "assets")
+  .bower()
+  .done()
+
+// Build specific assets.
+steamer.img
+  .copy("assets/test01/img", '/t01i')
+
+// Export the configuration.
+module.exports = steamer.steam()
