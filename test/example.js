5d2cc4fb9bb38127fd271ea72e007994ee740c16
Greg Payne <greg.payne@gmail.com>
2015-09-21 12:27:44 -0500

    Add build cli command to test

--- test/example.js
+++ test/example.js
@@ -1,16 +1,30 @@
 var vows = require('vows'),
     assert = require('assert'),
     fs = require('fs'),
-    path = require('path');
+    path = require('path')
+    exec = require('child_process').exec;
 
 function testExample(dir) {
   return {
-    'Folder' : {
+    'Folder Init' : {
       topic: function() {
         fs.stat(dir, this.callback)
       },
       "exists" : function(err, stat) {
         assert.isTrue(stat.isDirectory())
+      },
+      "Build" : {
+        topic: function() {
+          fs.symlinkSync(fs.realpath("node_modules", "dir/node_modules")
+          exec(dir + "/node_modules/broccoli-cli/bin/broccoli build " + dir + "/build", this.callback)
+        },
+        "output" : function(err, stdout, sterr) {
+          console.log('stdout: ' + stdout);
+          console.log('stderr: ' + stderr);
+          if (error !== null) {
+            console.log('exec error: ' + error);
+          }
+        }
       }
     }
   }
